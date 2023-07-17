6ba9db623375880f5d3acec75cbb2cf718a57670
Fabrice Couderc <fabrice.couderc@cern.ch>
2023-07-17 17:20:07 +0200

    setup env variables for different directories

---
+++ Trees2WS/setup_dirs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export fggDir="/afs/cern.ch/work/f/fcouderc/public/hgg/flashgg/CMSSW_10_6_29/src/"
+export storeDir="/eos/cms/store/group/dpg_ecal/comm_ecal/localreco/achgg/HiggsCouplings/Trees_2023_07_10_ACCats_VBF_VHHad"
+export wsDirMC=$storeDir/ws/signal/
+export wsDirDATA=$storeDir/ws/data/
